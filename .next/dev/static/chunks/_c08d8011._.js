9b2c91906a3cf6599a3ef215307123d31579f9ad
Gregory Hodge <GHodge.vi@gmail.com>
2026-02-27 16:46:01 -0400

    fix: add missing ts dependencies for vercel build

---
+++ .next/dev/static/chunks/_c08d8011._.js
@@ -0,0 +1,53 @@
+(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
+"[project]/components/AutoRefresh.tsx [app-client] (ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s([
+    "default",
+    ()=>AutoRefresh
+]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$index$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/compiled/react/index.js [app-client] (ecmascript)");
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$navigation$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/navigation.js [app-client] (ecmascript)");
+var _s = __turbopack_context__.k.signature();
+'use client';
+;
+;
+function AutoRefresh() {
+    _s();
+    const router = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$navigation$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["useRouter"])();
+    (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$index$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["useEffect"])({
+        "AutoRefresh.useEffect": ()=>{
+            // Check for new data every 60 seconds
+            const interval = setInterval({
+                "AutoRefresh.useEffect.interval": ()=>{
+                    router.refresh();
+                }
+            }["AutoRefresh.useEffect.interval"], 60000);
+            return ({
+                "AutoRefresh.useEffect": ()=>clearInterval(interval)
+            })["AutoRefresh.useEffect"];
+        }
+    }["AutoRefresh.useEffect"], [
+        router
+    ]);
+    return null;
+}
+_s(AutoRefresh, "vQduR7x+OPXj6PSmJyFnf+hU7bg=", false, function() {
+    return [
+        __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$navigation$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["useRouter"]
+    ];
+});
+_c = AutoRefresh;
+var _c;
+__turbopack_context__.k.register(_c, "AutoRefresh");
+if (typeof globalThis.$RefreshHelpers$ === 'object' && globalThis.$RefreshHelpers !== null) {
+    __turbopack_context__.k.registerExports(__turbopack_context__.m, globalThis.$RefreshHelpers$);
+}
+}),
+"[project]/node_modules/next/navigation.js [app-client] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+module.exports = __turbopack_context__.r("[project]/node_modules/next/dist/client/components/navigation.js [app-client] (ecmascript)");
+}),
+]);
+
+//# sourceMappingURL=_c08d8011._.js.map
